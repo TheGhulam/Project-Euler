0e595a847512b5047e7cd728b44197105b8627a2
gahme <g_ahmeddd@yahoo.com>
2021-06-01 09:18:38 +0500

    Problem 7

---
+++ p7.py
@@ -0,0 +1,28 @@
+from math import log, ceil
+
+
+# Using "Sieve of Erathosthenes"
+def find_primes(limit):
+    nums = [True] * (limit + 1)
+    nums[0] = nums[1] = False
+
+    for (i, is_prime) in enumerate(nums):
+        if is_prime:
+            yield i
+            for n in range(i * i, limit + 1, i):
+                nums[n] = False
+
+
+def upper_bound_for_p_n(n):
+    if n < 6:
+        return 100
+    return ceil(n * (log(n) + log(log(n))))
+
+
+def find_n_prime(n):
+    primes = list(find_primes(upper_bound_for_p_n(n)))
+    return primes[n-1]
+
+
+if __name__ == "__main__":
+    print(find_n_prime(10001))
