d798eb76630c9759692d11742e62b826f742e9c4
gahme <g_ahmeddd@yahoo.com>
2021-06-06 17:18:10 +0500

    p11

--- p11.py
+++ p11.py
@@ -27,64 +27,42 @@ The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally in the 20×20 grid?)
 """
 
-grid ='''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'''
+import numpy as np
 
-grid = grid.strip().split('\n') # Split the string in rows
-grid = [map(int, x.strip().split(' ')) for x in grid] # Split in columns and convert to int using map
+# Load each line into an array
+with open('p11.txt', 'r') as grid:
+    array = []
+    for line in grid:
+        array.append(line)
+# print(array)
 
-def greatest_product_in_matrix(matrix):
-    largest = 0
-    for i in matrix:
-        multiple = reduce(lambda x, y:x*y, i)
-        if multiple > largest:
-            largest = multiple
-    for j in xrange(len(matrix)):
-        multiple = 1
-        for k in xrange(len(matrix)):
-            multiple = multiple*matrix[k][k]
-        if multiple > largest:
-            largest = multiple
-    multiple = 1
-    right_dia = 1
-    for k in xrange(len(matrix)):
-        multiple = multiple*matrix[k][j]
-        right_dia *= matrix[len(matrix)-1-k][k]
-    if multiple > largest:
-        largest = multiple
-    if right_dia > largest:
-        largest = right_dia
-    return largest
+# Create new array that converts the number strings into integers
+int_array = []
+for i in array:
+    j = i.split(' ')
+    k = [int(n) for n in j]
+    int_array.append(k)
+# print(int_array)
 
-sub_matrices = []
+int_matrix = np.matrix(int_array)
+print(int_matrix)
 
-for i in range(len(grid)-3):
-    for j in range(len(grid[0])-3):
-        sub = []
-        sub.append(numbers[i][j:j+4])
-        sub.append(numbers[i+1][j:j+4])
-        sub.append(numbers[i+2][j:j+4])
-        sub.append(numbers[i+3][j:j+4])
-        a.append(sub)
-
-a = map(greatest_product_in_matrix,a)
-
-print(max(a))
+def search_for_max_product_in_4x4_matrix(m):
+    maxProd = 1
+    for i in range(16):
+        for j in range(16):
+            prod1 = m[i,j] * m[i+1,j] * m[i+2,j] * m[i+3,j]
+            if prod1 > maxProd:
+                maxProd = prod1
+            prod2 = m[i,j] * m[i,j+1] * m[i,j+2] * m[i,j+3]
+            if prod2 > maxProd:
+                maxProd = prod2
+            prod3 = m[i,j] * m[i+1,j+1] * m[i+2,j+2] * m[i+3,j+3]
+            if prod3 > maxProd:
+                maxProd = prod3
+            prod4 = m[19-i,j] * m[18-i,j+1] * m[17-i,j+2] * m[16-i,j+3]
+            if prod4 > maxProd:
+                maxProd = prod4
+    return maxProd
+if __name__ == "__main__":
+    print(search_for_max_product_in_4x4_matrix(int_matrix))
