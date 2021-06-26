5a0479c99ab9e6a851e32f9040d3e86b34df610f
gahme <g_ahmeddd@yahoo.com>
2021-06-26 13:06:37 +0500

    p31 - What an elegant solution

---
+++ p31.py
@@ -0,0 +1,9 @@
+target = 200
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+ways = [1] + [0]*target
+
+for c in coins:
+    for i in range(c, target+1):
+        ways[i] += ways[i-c]
+
+print("Ways to make change =", ways[target])
