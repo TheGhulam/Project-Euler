695c3fe23f7cfda08b7b437df87d532bac7b9354
gahme <g_ahmeddd@yahoo.com>
2021-06-29 19:04:54 +0500

    Got p35 refactored by a pro

--- p35.py
+++ p35.py
@@ -1,4 +1,7 @@
 from collections import deque
+import time
+
+start = time.time()
 
 def sieve(limit):
     nums = [True] * (limit+1)
@@ -12,26 +15,29 @@ def sieve(limit):
 
 def rotations(n):
     l = deque(list(str(n)))
-    r = []
+    # r = []
 
     for _ in range(len(l)):
-        r.append(int("".join(list(l))))
+        # r.append(int("".join(list(l))))
+        yield int("".join(list(l)))
         l.rotate()
-    return r
+    # return r
 
 def main(n):
     cp = []
-    s = list(sieve(n))
+    s = set(sieve(n))
     for i in s:
-        r = rotations(i)
-        flags = [False] * len(r)
-        for k, j in enumerate(r):
-            if j in s:
-                flags[k] = True
-        if all(flags):
+        # r = rotations(i)
+        # flags = [False] * len(r)
+        # for k, j in enumerate(r):
+        #     if j in s:
+        #         flags[k] = True
+        # if all(flags):
+        if all(j in s for j in rotations(i)):
             print(i)
             cp.append(i)
     return len(cp)
 
 
 print(main(1000000))
+print("Time taken: ", (time.time() - start))
