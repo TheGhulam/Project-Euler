17409c3545d432f81ee11b3674a59a3ab7e33646
gahme <g_ahmeddd@yahoo.com>
2021-06-30 16:41:51 +0500

    p36

---
+++ p36.py
@@ -0,0 +1,10 @@
+def palindromic(n):
+    if str(n) == str(n)[::-1]: return True
+    else: return False
+
+s = 0
+for i in range(1_000_000):
+    if palindromic(i) and palindromic(int(bin(i)[2:])):
+        s += i
+
+print(s)
