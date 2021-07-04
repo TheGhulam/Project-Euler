c4cc30aed1b9781cf1b6f15bb7df949b4aaf90f8
gahme <g_ahmeddd@yahoo.com>
2021-07-04 10:08:20 +0500

    p40

---
+++ p40.py
@@ -0,0 +1,7 @@
+num = ''
+for i in range(0, 1000001):
+    num += str(i)
+
+exp = int(num[1]) * int(num[10]) * int(num[100])* int(num[1000]) * int(num[10000]) * int(num[100000]) * int(num[1000000])
+
+print(exp)
