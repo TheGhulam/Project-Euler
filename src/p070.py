8bde031ad4ebecef8ded04dc7c25b208cf645de1
gahme <g_ahmeddd@yahoo.com>
2021-08-09 11:19:54 +0500

    p70

--- src/p070.py
+++ src/p070.py
@@ -1,39 +1,41 @@
 #! /usr/bin/env python
 
 
-from math import gcd
+from funcs import sieve
+from math import sqrt
 
-def a_perm_b_int(a, b):
-    return sorted(str(a)) == sorted(str(b))
+"""
+We can reduce our search significantly by selecting prime pairs (p_1, p_2)
+and calculate n as n = p_1 * p_2. This allows the totient to be calculated as
+phi(n) = (p_1 - 1)(p_2 - 1) for n. Now just find the minimum ratio for those
+n and phi(n) that are permutations.
+
+The range of primes was selected by taking the square root of the upper bound,
+10,000,000 which is about 3162 and taking ~30% for a range of primes from
+2000 to 4000 (247 primes).
+"""
+
+L = 10 ** 7
+primes = list(sieve(int(1.2 * sqrt(L))))
+del primes[: int(0.6 * len(primes))]
 
 
-def phi(n):
-    """Euler's totient function AKA phi
-
-    >>> phi(5)
-    4
-    """
-    amount = 0
-    for k in range(1, n + 1):
-        if gcd(n, k) == 1:
-            amount += 1
-    return amount
-
-
-
-def p70():
-    m_ratio = m_n = 0
-    for i in range(1, 10 ** 7):
-        print(i)
-        p = phi(i)
-        if a_perm_b_int(p, i):
-            ratio = i/p
-            if ratio > m_ratio:
-                m_ratio = ratio
-                m_n = i
-    return m_n
-
+def p70(limit):
+    min_q, min_n, i = 2, 0, 0
+    for p1 in primes:
+        i += 1
+        for p2 in primes[i:]:
+            if (p1 + p2) % 9 != 1:
+                continue
+            n = p1 * p2
+            if n > limit:
+                return min_n
+            phi = (p1 - 1) * (p2 - 1)
+            q = n / float(phi)
+            if sorted(str(phi)) == sorted(str(n)) and min_q > q:
+                min_q, min_n = q, n
+    return "Not found"
 
 
 if __name__ == "__main__":
-    print(p70())
+    print(p70(L))
